62a7f404e678384c5ea97e62a3a60c7b0b176337
ellenlowing <lowing@bu.edu>
2019-10-20 00:29:38 -0400

    first commit with basic layout of desktop

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>David Belt</title>
+    <link rel="stylesheet" href="assets/css/reset.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <main>
+      <div id="content-div-intro" class="content-div">
+        <div class="intro-title">David Belt</div>
+        <div class="intro-slideshow slideshow"></div>
+        <p class="intro-p">
+          David Belt is a founder, investor, and entrepreneur. <br><br>
+          Through DBI , Macro Sea , and New Lab , he has had the opportunity to accomplish great things with great people in New York and around the world.
+        </p>
+      </div>
+      <div id="content-div-projects" class="content-div">
+        <div class="content-div-title">Projects</div>
+        <div class="modules">
+          <div class="module">
+            <div class="module-slideshow slideshow"></div>
+            <div class="module-title">Education</div>
+            <p class="module-p">
+              Since 2012, DBI has cultivated a lasting relationship with Uncommon Schools, guiding the network’s development and operation of facilities to support their expansion across the United States.
+            </p>
+          </div>
+          <div class="module">
+            <div class="module-slideshow slideshow"></div>
+            <div class="module-title">Arts / Culture</div>
+            <p class="module-p">
+              As one of the final buildings in the redevelopment of the World Trade Center site, the The Perelman Center will activate the site throughout the day and into the evening with innovative programming.             </p>
+          </div>
+          <div class="module">
+            <div class="module-slideshow slideshow"></div>
+            <div class="module-title">Community</div>
+            <p class="module-p">
+              Now that we’ve made code-compliant, street-legal dumpster pools, Macro Sea is working to create cost-effective ways to bring pop-up pools to municipalities nationwide.            </p>
+          </div>
+          <div class="module">
+            <div class="module-slideshow slideshow"></div>
+            <div class="module-title">Investments</div>
+            <p class="module-p">
+              Through Exalt, teens and young adults are re-engaged with a love of learning and empowered to build fruitful lives outside of the criminal justice system.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div id="content-div-news" class="content-div">
+        <div class="content-div-title">News / Press</div>
+      </div>
+      <div id="content-div-contact" class="content-div">
+        <div class="content-div-title">Contact David Belt</div>
+      </div>
+    </main>
+  </body>
+</html>

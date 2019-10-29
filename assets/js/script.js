2607fd9725fd1534f4cc120132ad4b97c47b5b56
ellenlowing <lowing@bu.edu>
2019-10-29 17:23:32 -0400

    Add favicon + remove slidebars for menu + add hover transition to links (not yet svg)

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,21 +79,21 @@ window.onload = () => {
   });
 
   // Set up nav w/ slidebars
-  controller.init();
+  // controller.init();
   $('.hamburger-logo').click((e) => {
     e.stopPropagation();
     e.preventDefault();
     if($('.hamburger').hasClass('hamburger-active')) {
-      controller.close('id-hamburger');
+      // controller.close('id-hamburger');
       $('.hamburger').removeClass('hamburger-active');
     } else {
-      controller.open('id-hamburger');
+      // controller.open('id-hamburger');
       $('.hamburger').addClass('hamburger-active');
     }
   })
   $(document).click( (e) => {
     if($('.hamburger').hasClass('hamburger-active') && (e.pageX < ($(window).width() - $('.hamburger-menu').width()) || e.clientY > $('.hamburger-menu').height())) {
-      controller.close('id-hamburger');
+      // controller.close('id-hamburger');
       $('.hamburger').removeClass('hamburger-active');
     }
   });
